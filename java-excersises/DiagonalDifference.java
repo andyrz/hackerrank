213be7ca8d4f5cbc42a48f28a0877a4a901b765b
andyrz <33523859+andyrz@users.noreply.github.com>
2018-08-09 08:36:40 -0400

    Add files via upload

---
+++ java-excersises/DiagonalDifference.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class DiagonalDifference {
+
+        static int diagonalDifference(int[][] arr) {
+            int size = arr.length;
+            int ltrdiag = 0;
+            int rtldiag = 0;
+            for(int i = 0; i < size; i++){
+                ltrdiag += arr[i][i];
+                rtldiag += arr[i][size-i-1];
+            }
+            return Math.abs(ltrdiag - rtldiag);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("Diagonal Difference.txt"));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
